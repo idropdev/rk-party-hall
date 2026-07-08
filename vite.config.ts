15dee45375ca42ab70220d9470834bae3ed33ae0
Carlos Gonzalez <cgonzalez94@miners.utep.edu>
2026-07-08 16:19:59 -0600

    Fix GitHub Pages asset paths

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
 
   return {
-    base: mode === 'production' ? '/rk-party-hall/' : '/',
+    base: './',
     plugins: [
       react(),
       tailwindcss(),
